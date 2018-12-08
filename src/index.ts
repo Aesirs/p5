c51f90f285015b8972bfd84aad5e10f0341ca913
Benjamin Couvrette <benjamin.couvrette@hotmail.ca>
2018-12-08 17:56:18 -0500

    P5Setup

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+/// <reference path='../types/p5.global-mode.d.ts' />
+/// <reference path='sketch/sketch.ts' />
---
+++ src/sketch/sketch.ts
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+
+function setup() {
+    const canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('sketch');
+    stroke(255);
+}
+
+function draw() {
+    background(0);
+}
+
